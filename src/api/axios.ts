afcf32368d029752994da6069b01899390ec957c
toyjack <toyjack@gmail.com>
2022-12-12 10:53:11 +0900

    fixed: cors

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://portal.kojisho.com/api/v1',
-  // baseURL: 'http://127.0.0.1:8000/api/v1',
+  // baseURL: 'https://portal.kojisho.com/api/v1',
+  baseURL: 'http://127.0.0.1:8000/api/v1',
   timeout: 1000,
   headers: { 
-    // 'Content-Type':'application/json;charset=utf-8',
+    'Content-Type':'application/json;charset=utf-8',
     // 'Access-Control-Allow-Origin': '*',
    }
 });
