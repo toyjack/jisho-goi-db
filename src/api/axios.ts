797b4d915c10a9f0aaa89100a6541e498c855782
toyjack <toyjack@gmail.com>
2022-12-12 10:55:35 +0900

    fixed: api baseUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  // baseURL: 'https://portal.kojisho.com/api/v1',
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://portal.kojisho.com/api/v1',
+  // baseURL: 'http://127.0.0.1:8000/api/v1',
   timeout: 1000,
   headers: { 
     'Content-Type':'application/json;charset=utf-8',
