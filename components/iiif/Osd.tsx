043ebeaedc5e50fb055c749f7bae9f57f19fb6f8
Guanwei Liu <toyjack@gmail.com>
2023-05-21 16:36:58 +0900

    working on iiif viewer

--- components/iiif/Osd.tsx
+++ components/iiif/Osd.tsx
@@ -1,33 +1,36 @@
 "use client"
-import OpenSeadragon, { Options } from "openseadragon";
-import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import  OpenSeadragon,{ Options } from "openseadragon";
+import { useEffect, useState } from "react";
 
 export interface OsdProps {
   tiles: string[];
 }
 function Osd({tiles}: OsdProps) {
-  const randomId = Math.random().toString(36).substring(7);
 
-  const options: Options = {
-    sequenceMode: true,
-    id: randomId,
-    tileSources: tiles,
-  };
+  const randomId = Math.random().toString(36).substring(7);
+  const [viewer, setViewer]= useState<OpenSeadragon.Viewer | null>(null);
+
+  const initViewer = () => {
+    viewer && viewer.destroy();
+    setViewer(OpenSeadragon({
+      id: randomId,
+      sequenceMode: true,
+      // tileSources: tiles,
+    }))
+  }
 
   useEffect(() => {
-    const viewer = OpenSeadragon(options);
+    initViewer();
     return () => {
-      viewer.destroy();
+      viewer?.destroy();
     };
-  }, [options]);
-
+  }, [])
 
   return (
     <>
-      <div>OSD</div>
-      <div>
-        <div id={randomId} className="w-full h-full z-0 relative"></div>
-      </div>
+      <div id={randomId} className="w-full h-96 relative bg-inherit"></div>
+      <pre>{JSON.stringify(tiles, null, 2)}</pre>
     </>
   );
 }
