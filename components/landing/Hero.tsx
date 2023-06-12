cdab37c75149f3f9773f662552f38674f3158794
Guanwei Liu <toyjack@gmail.com>
2023-06-12 11:37:35 +0900

    update 助成

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 function Hero() {
@@ -14,8 +15,28 @@ function Hero() {
           <h1 className="mb-5 text-5xl font-bold">辞書語彙データベース</h1>
           <p className="mb-5">
             科研費プロジェクト：
-            『色葉字類抄』の語彙研究および総合データベースの構築
-            異種古辞書間におけるデータ連携モデルの構築
+            <Link
+              className="link link-hover"
+              href={"https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-21H00529"}
+              target="_blank"
+            >
+              『色葉字類抄』の語彙研究および総合データベースの構築（科研費21H00529）
+            </Link>
+            、 
+            <Link
+              className="link link-hover"
+              href={"https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-21K18364"}
+              target="_blank"
+            >
+              異種古辞書間におけるデータ連携モデルの構築（科研費21K18364）
+            </Link>、
+            <Link
+              className="link link-hover"
+              href={"https://www.inamori-f.or.jp/inamori_grants_list/2021"}
+              target="_blank"
+            >
+              日本初の国語辞書『色葉字類抄』に採録された漢籍出典語彙の院政期における使用状況（2021年度稲盛研究助成）
+            </Link>
           </p>
           <a className="btn btn-primary" href="#databases">
             データベース一覧
