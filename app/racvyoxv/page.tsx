8ffd0e9ebe0a2eef10222137b7b3525f20396158
toyjack <toyjack@gmail.com>
2023-08-18 23:44:58 +0900

    update racvyoxv page

--- app/racvyoxv/page.tsx
+++ app/racvyoxv/page.tsx
@@ -4,7 +4,6 @@ import RacvyoxvManual from "@/markdown/RacvyoxvManual.mdx"
 function RacvyoxvPage() {
   return (
     <div className="px-4 pb-8">
-      <Shikouban />
 
       <article className="max-w-none prose mx-auto p-4">
         <RacvyoxvManual />
