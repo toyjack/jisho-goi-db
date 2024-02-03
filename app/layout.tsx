50b8c21d3403a1d0ce29f4ba951b69d49adb8d5e
toyjack <toyjack@gmail.com>
2024-02-03 11:59:49 +0900

    fixing g-tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import CommonHeader from "@/components/common/Header";
 import CommonFooter from "@/components/common/Footer";
 import GoogleAnalytics from "@/components/common/GoogleAnalytics";
 import { Providers } from "./providers";
-import { InspectOptions } from "util";
 
 const title =
   "辞書語彙データベース" + (process.env.IS_DEV === "true" ? "（検証用）" : "");
@@ -30,6 +29,7 @@ export default function RootLayout({
       <head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta charSet="utf-8" />
+        <GoogleAnalytics />
       </head>
       <body>
         <Providers>
@@ -38,7 +38,6 @@ export default function RootLayout({
         </Providers>
         <CommonFooter />
         <VercelAnalytics />
-        <GoogleAnalytics />
       </body>
     </html>
   );
--- components/common/GoogleAnalytics.tsx
+++ components/common/GoogleAnalytics.tsx
@@ -1,9 +1,23 @@
 "use client";
-import { GA_MEASUREMENT_ID } from "@/lib/gtag";
+
+import { GA_MEASUREMENT_ID, existsGaId, pageview } from "@/lib/gtag";
+import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function GoogleAnalytics() {
   const gaId = GA_MEASUREMENT_ID;
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+    const url = pathname + searchParams.toString()
+    pageview(url)
+  }, [pathname, searchParams])
 
   return (
     <>
@@ -11,7 +25,8 @@ export default function GoogleAnalytics() {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
       ></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
+
+      <Script id='gtag-init' strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
