3f1caefb5611b6dc1eaddc58e38790fcba166bb6
劉冠偉 <liuguanwei@ryuukaninoMacBook-Pro.local>
2024-05-02 16:46:33 +0900

    updated landing page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,29 +1,26 @@
 import ShukaiSection from "@/components/landing/Shukai";
 import Shukai2024Section from "@/components/landing/Shukai-2024";
-import client from "@/tina/__generated__/client";
+// import client from "@/tina/__generated__/client";
 
 async function NewsPage() {
-  const newsResponse = await client.queries.newsConnection({
-    last:1
-  })
-  const allNews = newsResponse.data.newsConnection.edges?.map(news => {
-    return {
-      title: news?.node?.title,
-      date: news?.node?.date,
-      url: news?.node?._sys.filename,
-      relativePath: news?.node?._sys.relativePath,
-      news: news?.node
-    }
-  })
+  // const newsResponse = await client.queries.newsConnection({
+  //   last:1
+  // })
+  // const allNews = newsResponse.data.newsConnection.edges?.map(news => {
+  //   return {
+  //     title: news?.node?.title,
+  //     date: news?.node?.date,
+  //     url: news?.node?._sys.filename,
+  //     relativePath: news?.node?._sys.relativePath,
+  //     news: news?.node
+  //   }
+  // })
 
-  const lastNews = allNews![0].news
+  // const lastNews = allNews![0].news
 
   // TODO on working
   return (
     <>
-      {/* <pre>
-        {JSON.stringify(lastNews, null, 2)}
-      </pre> */}
       <Shukai2024Section />
       <div className="divider"></div>
       <ShukaiSection />
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import Hero from '@/components/landing/Hero';
 import NewsSection from '@/components/landing/NewsSection';
 import DatabasesSection from "@/components/landing/DatabasesSection";
 import MembersSection from '@/components/landing/MembersSection';
-import Shukai2024Section from '@/components/landing/Shukai-2024';
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Shukai2024Section />
       <NewsSection />
       <DatabasesSection />
       <MembersSection />
