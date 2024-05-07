c1c31985864a8e3fb6d1f88732c7c15ce1670547
toyjack <toyjack@gmail.com>
2024-05-07 21:44:31 +0900

    fix bug for kwrs

--- app/kwrs/results/layout.tsx
+++ app/kwrs/results/layout.tsx
@@ -3,7 +3,7 @@ import client from "@/tina/__generated__/client";
 
 async function layout({ children }: { children: React.ReactNode }) {
   const result = await client.queries.manual({
-    relativePath: "hzwm.mdx",
+    relativePath: "kwrs.mdx",
   });
   
   return (
