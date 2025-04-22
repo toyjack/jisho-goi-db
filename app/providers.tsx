2f280524cf8a25d497e15b517afe3c9419e9bc30
toyjack <toyjack@gmail.com>
2025-04-22 14:24:19 +0900

    remove unnecessary lib: nextui

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
