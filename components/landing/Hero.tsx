e5f6795678e0bc5c0311b1344364f6a33a803de4
toyjack <toyjack@gmail.com>
2025-07-05 16:41:24 +0900

    Add additional research links to Hero component for enhanced resource access

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -14,6 +14,27 @@ function Hero() {
         <div className="max-w-lg">
           <h1 className="mb-5 text-5xl font-bold">辞書語彙データベース</h1>
           <p className="mb-5">
+            <Link
+              className="link link-hover"
+              href={"https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-25K21858"}
+              target="_blank"
+            >
+              辞書語彙と文章語彙の重なりについての通時的研究（科研費25K21858）
+            </Link>
+            <Link
+              className="link link-hover"
+              href={"https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-25K00466"}
+              target="_blank"
+            >
+              古辞書コーパスの構築―通時語彙検索の実現を目指して―（科研費25K00466）
+            </Link>
+            <Link
+              className="link link-hover"
+              href={"https://kaken.nii.ac.jp/ja/grant/KAKENHI-PROJECT-24K16080"}
+              target="_blank"
+            >
+              日本古辞書ポータルサイトの開発による公開研究資源の高度利用（科研費24K16080）
+            </Link>
             <span className="text">
               「清华大学大学自主科研计画文科專项经费·基研究專项(W01)《色叶字类抄》为中心的日本古辞书研究与数据库建设」（2023THZWJC31·藤本灯）
             </span>
