c01cd869951f0a40f237f730846cf303ad5f6eee
toyjack <toyjack@gmail.com>
2026-02-20 14:17:14 +0900

    研究集会２０２５を追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Hero from '@/components/landing/Hero';
 import NewsSection from '@/components/landing/NewsSection';
 import DatabasesSection from "@/components/landing/DatabasesSection";
 import MembersSection from '@/components/landing/MembersSection';
+import Shukai2025Section from '@/components/landing/Shukai-2025';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Shukai2025Section />
       <NewsSection />
       <DatabasesSection />
       <MembersSection />
---
+++ components/landing/Shukai-2025.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import shukaiPoster from "/public/images/shukai-2025.png";
+
+function Shukai2025Section() {
+  return (
+    <div className="hero min-h-screen bg-base-100">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Link href={shukaiPoster.src} target="_blank">
+          <Image
+            src={shukaiPoster}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="poster-2022"
+          />
+        </Link>
+        <div>
+          <h1 className="text-5xl font-bold">
+            研究集会 <br />
+            古辞書・漢字音研究とデータベース2025
+          </h1>
+          <p className="py-6">
+            開催日: <span className="text-red-600 font-bold">2026年3月1日（日）13:00～16:30 </span> <br />
+            開催地: 【オンライン】Zoom　 <br />
+            お問い合わせ: akari[atmark]tsinghua.edu.cn
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Shukai2025Section
